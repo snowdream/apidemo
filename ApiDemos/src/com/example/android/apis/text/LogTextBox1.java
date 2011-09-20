05e3813b8f76843bec83746a244f9beebd2c9928
snowdream <yanghui1986527@gmail.com>
2011-09-21 03:08:07 +0800

    ADD    2.2    LogTextBox

--- ApiDemos/src/com/example/android/apis/text/LogTextBox1.java
+++ ApiDemos/src/com/example/android/apis/text/LogTextBox1.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.apis.text;
 
 import com.example.android.apis.R;
@@ -23,11 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Using a LogTextBox to display a scrollable text area
- * to which text is appended.
- *
- */
+
 public class LogTextBox1 extends Activity {
     
     private LogTextBox mText;
@@ -40,11 +20,20 @@ public class LogTextBox1 extends Activity {
         
         mText = (LogTextBox) findViewById(R.id.text);
         
+        //对Button的监听方法一：设置监听器
         Button addButton = (Button) findViewById(R.id.add);
         addButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 mText.append("This is a test\n");
             } });
+        
+        
+    }
+    
+    //对Button的监听方法二：自定义监听方法，需要设置android:onClick属性
+    //这个方法必须是Public类型的，并且只能接受view作为唯一参数 
+    public void selfDestruct(View view) {
+        mText.append("This is a test\n");
     }
 }
