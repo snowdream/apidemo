83f6ff57766ae982d160e4b321eabe0515c3b1d6
snowdream <yanghui1986527@gmail.com>
2011-09-24 03:07:30 +0800

    ADD     3.1     Buttons

--- ApiDemos/src/com/example/android/apis/view/Buttons1.java
+++ ApiDemos/src/com/example/android/apis/view/Buttons1.java
@@ -1,39 +1,54 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.apis.view;
 
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
+
 import com.example.android.apis.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 
-/**
- * A gallery of the different styles of buttons.
- */
+
 public class Buttons1 extends Activity {
-
+    //声明ToggleButton对象  
+	private ToggleButton mtoggleBtn = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buttons_1);
+              
+        //通过findViewById获得ToggleButton  
+        mtoggleBtn = (ToggleButton)findViewById(R.id.button_toggle);
+        
+        //点击监听
+        mtoggleBtn.setOnClickListener(new ToggleButton.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO 点击按键时触发响应
+				if(mtoggleBtn.isChecked()){
+					//当按键被按下，处于选中状态时，执行此处定义的动作
+				}
+				else{
+					//当按键被未被按下，处于未选中状态时，执行此处定义的动作
+				}
+			}
+		});
+        
+        //状态改变监听
+        mtoggleBtn.setOnCheckedChangeListener(new ToggleButton.OnCheckedChangeListener() {
+			
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO 状态改变时触发响应
+				if(isChecked){
+					//当按键被按下，处于选中状态时，执行此处定义的动作
+				}
+				else{
+					//当按键被未被按下，处于未选中状态时，执行此处定义的动作
+				}				
+			}
+        });
     }
 }
