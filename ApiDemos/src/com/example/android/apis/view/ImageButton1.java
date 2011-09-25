15c561655e6ee4f65ef84179249822e6a16f3f41
snowdream <yanghui1986527@gmail.com>
2011-09-26 01:08:50 +0800

    ADD     3.2     ImageButton

--- ApiDemos/src/com/example/android/apis/view/ImageButton1.java
+++ ApiDemos/src/com/example/android/apis/view/ImageButton1.java
@@ -1,33 +1,26 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.apis.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 
 import com.example.android.apis.R;
 
 
 public class ImageButton1 extends Activity {
-    
+    private ImageButton mImageButton = null;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_button_1);
+  
+        //通过findViewById获得ImageButton          
+        mImageButton = (ImageButton)findViewById(R.id.myImageButton01);
+        
+        //引用android内置图标，作为ImageButton的按钮图标 
+        mImageButton.setImageResource(android.R.drawable.sym_action_call);
+        
     }
 }
