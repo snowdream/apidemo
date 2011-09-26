26953901e5e8c058efda17d4d458a9cfcb13dafd
snowdream <yanghui1986527@gmail.com>
2011-09-27 00:19:00 +0800

    ADD     3.3     Visibility

--- ApiDemos/src/com/example/android/apis/view/Visibility1.java
+++ ApiDemos/src/com/example/android/apis/view/Visibility1.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.apis.view;
 
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
+
 import com.example.android.apis.R;
 
 import android.app.Activity;
@@ -28,46 +11,50 @@ import android.widget.Button;
 
 
 /**
- * Demonstrates making a view VISIBLE, INVISIBLE and GONE
+ *  设置一个View对象可见，不可见，彻底消失的实例演示
  *
  */
 public class Visibility1 extends Activity {
 
-    private View mVictim;
+    private View mVictim = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visibility_1);
 
-        // Find the view whose visibility will change
+        // 通过findViewById获得一个待改变可见性的View对象
         mVictim = findViewById(R.id.victim);
 
-        // Find our buttons
+        // 通过findViewById获得三个Button对象
         Button visibleButton = (Button) findViewById(R.id.vis);
         Button invisibleButton = (Button) findViewById(R.id.invis);
         Button goneButton = (Button) findViewById(R.id.gone);
 
-        // Wire each button to a click listener
+        // 给每个Button添加点击监听器
         visibleButton.setOnClickListener(mVisibleListener);
         invisibleButton.setOnClickListener(mInvisibleListener);
         goneButton.setOnClickListener(mGoneListener);
     }
 
+
     OnClickListener mVisibleListener = new OnClickListener() {
         public void onClick(View v) {
+        	  //设置mVictim可见
             mVictim.setVisibility(View.VISIBLE);
         }
     };
 
     OnClickListener mInvisibleListener = new OnClickListener() {
         public void onClick(View v) {
+      	  	//设置mVictim不可见       	
             mVictim.setVisibility(View.INVISIBLE);
         }
     };
 
     OnClickListener mGoneListener = new OnClickListener() {
         public void onClick(View v) {
+      	  	//设置mVictim彻底隐藏      	        	
             mVictim.setVisibility(View.GONE);
         }
     };
