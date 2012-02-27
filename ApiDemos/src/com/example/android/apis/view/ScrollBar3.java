47896e9a2358af7557eaccf90b9d5c5f2a772736
snowdream <yanghui1986527@gmail.com>
2012-02-27 15:43:16 +0800

    ADD     3.12     ScrollBars

--- ApiDemos/src/com/example/android/apis/view/ScrollBar3.java
+++ ApiDemos/src/com/example/android/apis/view/ScrollBar3.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.apis.view;
 
 import android.app.Activity;
@@ -30,6 +14,7 @@ public class ScrollBar3 extends Activity {
 
         setContentView(R.layout.scrollbar3);
         
+        // 通过findViewById方法获得一个id为view3的ScrollView控件，并设置滚动条类型为 View.SCROLLBARS_INSIDE_INSET 
         findViewById(R.id.view3).setScrollBarStyle(View.SCROLLBARS_INSIDE_INSET);
     }
 }
